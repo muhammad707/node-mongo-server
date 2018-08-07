9e71c982310b181d9bc9487270b7f6583ea2dd39
MSM_707 <sirojiddinov.1881@mail.ru>
2018-08-07 19:24:20 +0500

    Test GET/todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,10 +4,21 @@ var request = require('supertest'); //supertest library
 var {Todo} = require('./../models/todos'); //importing todo model
 var {app}  = require('./../server');
 
+// temporary  todos array
+var todos = [{
+		text: 'First test todo'
+	},
+	{
+		text: 'Second test todo'
+	}
+];
 beforeEach((done) => {
-	Todo.remove({}).then(() => done());
-})
+	Todo.remove({}).then(() =>{
+		return Todo.insertMany(todos);
+	}).then(() => done());
+});
 
+//*******************************TESTING POST REQUEST**********************************
 describe('POST/todos',() => {
 	it('should create new todo', (done) => {
 		var text = 'Testing to do text';
@@ -23,11 +34,47 @@ describe('POST/todos',() => {
 			if (err) {
 				return done(err);
 			}
-			Todo.find().then((todos) => {
+			Todo.find({text}).then((todos) => {
 				expect(todos.length).toBe(1);
 				expect(todos[0].text).toBe(text);
 				done();
 			}).catch((e) => done(e));
 		});
 	});
+
+	it('should not send post request', (done) => {
+		request(app)
+		.post('/todos')
+		.send({})
+		.expect(400)
+		.end((err, res) => {
+			if (err) {
+				return done(err);
+			}
+
+			Todo.find().then((todos) => {
+				expect(todos.length).toBe(2);
+				done();
+			}).catch((e) => {
+				done(e);
+			});
+
+		});
+	});
+});  // END OF POST REQUEST TESTING
+
+
+/******************************TESTING GET REQUEST **********************************/
+
+describe('GET/todos', () =>{
+	it('should get all todos from db', (done) => {
+		request(app)
+		 .get('/todos')
+		 .expect(200)
+		 .expect((res) => {
+		 	expect(res.body.todos.length).toBe(2);
+		 })
+		 .end(done);
+	})
 });
+
