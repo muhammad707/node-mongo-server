4261807ff57bfa4ab0b53a72e1eb3de356a05419
MSM_707 <sirojiddinov.1881@mail.ru>
2018-08-08 14:37:59 +0500

    Add delete script

--- server/server.js
+++ server/server.js
@@ -1,24 +1,24 @@
 var express = require('express'); // express library
 var bodyParser = require('body-parser'); //body parser library
-var { ObjectID} = require('mongodb');
+var { ObjectID } = require('mongodb');
 var { mongoose } = require('./db/mongodb'); // object destructuring that only stores mongoose property
 var { Todo } = require('./models/todos'); // stores todo model
 var { User } = require('./models/user'); //stores user model
 
-const port = process.env.PORT || 4000;
+const port =  3000;
 var app = express(); // installing route
 
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
 	 var todo = new Todo({
-	 	text: req.body.text
+		text: req.body.text
 	 });
 
 	 todo.save().then((doc) => {
-	 	res.send(doc);
+		res.send(doc);
 	 }, (e) => {
-	 	res.status(400).send(e);
+		res.status(400).send(e);
 	 });
 
 }); // End of Post request
@@ -49,6 +49,27 @@ app.get('/todos/:id', (req, res) => {
 	}).catch((e) => {
 		res.status(400).send();
 	});
+});
+
+// Deleting todo by id 
+
+app.delete('/todos/:id', (req, res) => {
+	var id = req.params.id;
+	if (!ObjectID.isValid(id)) {
+		return res.status(404).send();
+	}
+
+	Todo.findByIdAndRemove(id).then((todo) => {
+		if (!todo) {
+			return res.status(404).send();
+		}
+
+		res.send({todo});
+	}).catch((e) => {
+		res.status(400).send();
+	});
+
+
 });
 
 // Port 
